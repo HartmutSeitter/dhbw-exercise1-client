ad1ad9a9eba85c690ed5ae61d7c2b12a9656dfdd
Hartmut Seitter <hartmut.seitter@googlemail.com>
2020-11-30 15:11:35 +0100

    app.js hat einen neuen text bekommen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import PostList from './PostList';
 export default () => {
   return (
     <div className="container">
-      <h1>Create Posts</h1>
+      <h1>Create Posts am Montag 30.11.2020</h1>
       <PostCreate />
       <hr />
       <h1>Posts</h1>
